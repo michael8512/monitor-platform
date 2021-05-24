d097e978d11c4194a0d080911adb56747c319355
linshenghui <linshenghui321@sina.com>
2021-05-24 23:41:39 +0800

    WIP: 修改tabs

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,7 @@ export default {
 		width: 100%;
 		height: 100%;
 		overflow: hidden;
+    background-image: url('./pages/common/images/bg.png');
 	}
 }
 ::-webkit-scrollbar{
--- src/pages/common/page-head.vue
+++ src/pages/common/page-head.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="page-head">
+    <div class="page-title">智慧工地可视化监管平台</div>
     <div class="tabs">
-      <div
-        class="tabs-item"
-        v-for="(item, index) in tabs"
-        :key="index"
-        :class="item.label === '智慧工地可视化监管平台' ? 'page-title':''"
-        @click="gotoPage(item)"
-      >
-        <div :class="item.isActive ? 'tab-text-active': 'tab-text'">
-          <router-link :to="item.route">{{item.label}}</router-link>
+      <div class="tabs-left">
+        <div
+          class="tabs-left-item"
+          v-for="(item, index) in leftTabs"
+          :key="index"
+          @click="gotoPage(item, 'left')"
+        >
+          <div :class="item.isActive ? 'tab-text-active': 'tab-text'">
+            <router-link :to="item.route">{{item.label}}</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="tabs-right">
+        <div
+          class="tabs-right-item"
+          v-for="(item, index) in rightTabs"
+          :key="index"
+          @click="gotoPage(item, 'right')"
+        >
+          <div :class="item.isActive ? 'tab-text-active': 'tab-text'">
+            <router-link :to="item.route">{{item.label}}</router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -26,24 +40,56 @@ export default {
         { label: '项目概况', isActive: true, route: '/static' },
         { label: '视频实时监控', isActive: false, route: '/video' },
         { label: '扬尘监控', isActive: false, route: '/dust' },
-        { label: '车辆未冲', isActive: false, route: '/car' },
+        { label: '车辆未冲洗抓拍', isActive: false, route: '/car' },
         { label: '吊塔安全', isActive: false, route: '/crane' },
         { label: '施工升降机', isActive: false, route: '/lift' },
-        { label: '智慧工地可视化监管平台', isActive: false, route: '/home' },
-        { label: '安全检查', isActive: false, route: '/safety' },
-        { label: '人员定位', isActive: false, route: '/person' },
         { label: '卸料平台', isActive: false, route: '/dischange' },
-        { label: '临边防护', isActive: false, route: '/guard' },
-        { label: '实名制考勤', isActive: false, route: '/attendance' },
-        { label: '', isActive: false, route: '' },
+        { label: '智慧工地可视化监管平台', isActive: false, route: '/home' },
+        { label: '人员在岗', isActive: false, route: '/working' },
+        { label: '卸料平台', isActive: false, route: '/dischange' },
+        { label: '安全检查', isActive: false, route: '/safety' },
+        { label: '安全教育', isActive: false, route: '/education' },
+        { label: '基坑监测', isActive: false, route: '/monitor' },
+        { label: '高支模监测', isActive: false, route: '/model' },
+        { label: '消息中心', isActive: false, route: '/message' },
+      ],
+      leftTabs: [
+        { label: '项目概况', isActive: true, route: '/static' },
+        { label: '视频实时监控', isActive: false, route: '/video' },
+        { label: '扬尘监控', isActive: false, route: '/dust' },
+        { label: '车辆未冲洗抓拍', isActive: false, route: '/car' },
+        { label: '吊塔安全', isActive: false, route: '/crane' },
+        { label: '施工升降机', isActive: false, route: '/lift' },
+        { label: '卸料平台', isActive: false, route: '/dischange' },
+      ],
+      rightTabs: [
+        { label: '人员在岗', isActive: false, route: '/working' },
+        { label: '卸料平台', isActive: false, route: '/dischange' },
+        { label: '安全检查', isActive: false, route: '/safety' },
+        { label: '安全教育', isActive: false, route: '/education' },
+        { label: '基坑监测', isActive: false, route: '/monitor' },
+        { label: '高支模监测', isActive: false, route: '/model' },
+        { label: '消息中心', isActive: false, route: '/message' },
       ]
     }
   },
   methods: {
-    gotoPage({label}) {
-      this.tabs.forEach(item=>{
-        item.isActive = item.label === label ? true : false;
-      })
+    gotoPage({label}, direction) {
+      if (direction === 'left') {
+        this.leftTabs.forEach(item=>{
+          item.isActive = item.label === label ? true : false;
+        });
+        this.rightTabs.forEach(item=>{
+          item.isActive = false;
+        })
+      } else {
+        this.rightTabs.forEach(item=>{
+          item.isActive = item.label === label ? true : false;
+        })
+        this.leftTabs.forEach(item=>{
+          item.isActive = false;
+        })
+      }
     }
   }
 }
@@ -62,47 +108,82 @@ export default {
 
   .tabs {
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
 
-    &-item:not(.page-title) {
-      margin-top: 6rem;
-    }
-
-    &-item a {
+    a {
+      position: relative;
+      line-height: 3.1rem;
+      z-index: 2;
       text-decoration: none;
       display: block;
-      min-width: 8rem;
-      margin: 0 1.2rem;
-      line-height: 2.9rem;
-      font-family: MicrosoftYaHei;
-      font-size: 1.8rem;
-      color: rgba(179, 179, 179, 1);
-      cursor: pointer;
-      transition: all 0.2s;
+      position: relative;
+      font-size: 1.4rem;
+      font-family: AlibabaPuHuiTi-Regular, AlibabaPuHuiTi;
+      font-weight: 400;
+      color: #FFFFFF;
+      
+    }
+
+    &-left, &-right {
+      display: flex;
+      margin-left: 3rem;
+      &-item {
+        margin-top: 6rem;
+        position: relative;
+        width: 13rem;
+        height: 3.1rem;
+        text-align: center;
+        margin-left: -2.9rem;
+
+        &::after {
+          content: '';
+          background: url('./images/tab-bg.png');
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 13rem;
+          height: 3.1rem;
+        }
+      }
+    }
+
+    &-right {
+      margin-right: 1rem;
+      &-item::after {
+        transform: rotateY(180deg);
+      }
+      .tab-text-active::after {
+        transform: rotateY(180deg);
+      }
     }
   }
 
-  .tab-text a{
-    text-decoration: none;
-    display: block;
-    position: relative;
-    font-size: 1.6rem;
-    font-family: AlibabaPuHuiTi-Regular, AlibabaPuHuiTi;
-    font-weight: 400;
-    color: #FFFFFF;
-    line-height: 2.2rem;
+  .tab-text-active a {
+    color: rgba(25, 166, 231, 1);
+    &::after {
+      content: '';
+      background: url('./images/tab-bg-hover.png');
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 13rem;
+      height: 3.1rem;
+      z-index: 4;
+    }
   }
 
-  .page-title a{
-    display: block;
+  .page-title {
+    position: absolute;
+    left: 50%;
+    top: 1rem;
     line-height: 6rem;
-    margin: 0 4rem;
     font-family: FZZYJW--GB1-0;
     font-size: 4rem;
     color:rgba(255,255,255,1);
     background: linear-gradient(180deg, #FFFFFF 0%, #1272CC  100%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+    transform: translate(-50%, 0);
   }
 }
 </style>
